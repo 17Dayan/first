70141657a41165f863f946293c4e899005f71aac
dayanvilla <karoldayanvilla@gmail.com>
2022-05-16 16:03:11 -0500

    Primer commit.

---
+++ App.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, { Component } from 'react';
+import { View, styleSheet } from 'react-native';
+
+function App() {
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.containerBlue}/>
+      <View style={styles.containerGreen}/>
+
+    </View>
+    </View >
+  );
+
+}
+
+
+export default App;
